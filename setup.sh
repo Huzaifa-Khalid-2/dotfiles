596cd1208e766b3037df54cb40bd87b45ae7260d
Huzaifa-Khalid-2 <99287277+Huzaifa-Khalid-2@users.noreply.github.com>
2022-09-20 09:30:58 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -20,3 +20,33 @@ git config --global user.email "Huzaifa-Khalid-2"
 git config --global init.defaultBranch main
 git config -- global pull.rebase true
 git config -- global fetch.rebase true
+
+# Create folders
+mkdir /ICS4U
+mkdir /ICS4U/unit1
+mkdir /ICS4U/unit1/unit1-01
+mkdir /ICS4U/unit1/unit1-02
+mkdir /ICS4U/unit1/unit1-03
+mkdir /ICS4U/unit1/unit1-04
+mkdir /ICS4U/unit1/unit1-05
+mkdir /ICS4U/unit1/unit1-06
+mkdir /ICS4U/unit1/unit1-07
+mkdir /ICS4U/unit2
+mkdir /ICS4U/unit2/unit2-01
+mkdir /ICS4U/unit2/unit2-02
+mkdir /ICS4U/unit2/unit2-03
+mkdir /ICS4U/unit2/unit2-04
+mkdir /ICS4U/unit2/unit2-05
+mkdir /ICS4U/unit2/unit2-06
+mkdir /ICS4U/unit2/unit2-07
+mkdir /ICS4U/unit2/unit2-08
+mkdir /ICS4U/unit2/unit2-09
+mkdir /ICS4U/unit2/unit2-10
+mkdir /ICS4U/unit3/unit3-01
+mkdir /ICS4U/unit3/unit3-01
+mkdir /ICS4U/unit3/unit3-02
+mkdir /ICS4U/unit3/unit3-03
+mkdir /ICS4U/unit3/unit3-04
+mkdir /ICS4U/unit4
+mkdir /ICS4U/unit4/unit4-01
+mkdir /ICS4U/unit4/unit4-02
